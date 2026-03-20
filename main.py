db20a0e07fb52701465befda17894b99faab0454
Joshua Silva <joshuasilva414@gmail.com>
2026-03-20 11:07:15 -0500

    Basic facial recognition setup

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from daiquiri!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,5 @@
+import cv2
+import face_recognition
+import face_recognition_models
+
+video_capture = cv2.VideoCapture(0)
